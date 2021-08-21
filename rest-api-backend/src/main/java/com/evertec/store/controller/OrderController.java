8b69c13c93ab67c28bd1d66be047ef17adc7fdeb
Manuel Osorio <manuelosorio@usantotomas.edu.co>
2021-08-21 01:41:40 -0500

    update repository -> service

--- rest-api-backend/src/main/java/com/evertec/store/controller/OrderController.java
+++ rest-api-backend/src/main/java/com/evertec/store/controller/OrderController.java
@@ -3,11 +3,10 @@ package com.evertec.store.controller;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import javax.ws.rs.Consumes;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,29 +16,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.evertec.store.persistence.entity.Order;
 import com.evertec.store.persistence.repository.OrderRepository;
 import com.evertec.store.rest.validator.OrderRequestValidator;
+import com.evertec.store.service.OrderService;
 
-@Consumes("application/json")
 @RestController
-@RequestMapping("/order")
+@RequestMapping(path = "/order", 
+consumes = MediaType.APPLICATION_JSON_VALUE, 
+produces = MediaType.APPLICATION_JSON_VALUE)
 public class OrderController {
 
 	Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 	
-	@Autowired
-	private OrderRepository orderRepository;
+
 
 	@Autowired
-	private OrderRequestValidator orderRequestValidator;
+	private OrderService orderService;
 
-	@InitBinder(value = "orderRequest")
-	void initOrderRequestValidator(WebDataBinder binder) {
-		binder.setValidator(orderRequestValidator);
-	}
+//	@InitBinder(value = "orderRequest")
+//	void initOrderRequestValidator(WebDataBinder binder) {
+//		binder.setValidator(orderRequestValidator);
+//	}
+
 
 	@PostMapping()
 	public String createOrder() {
@@ -49,15 +51,13 @@ public class OrderController {
 	@GetMapping("/{id}")
 	public ResponseEntity<Order> getOrderById(@PathVariable(value = "id") Long orderId) {
 		logger.info("query order:" + orderId);
-		Order order = orderRepository.findById(orderId)
-				.orElseThrow(() -> new NoSuchElementException("Order not availbele for Id :" + orderId));
-
+		Order order = orderService.read(orderId);
 		return ResponseEntity.ok().body(order);
 	}
 
 	@GetMapping()
 	public List<Order> getAllOrders() {
-		return  orderRepository.findAll();		
+		return  orderService.read();		
 	}
 
 	@PutMapping()
