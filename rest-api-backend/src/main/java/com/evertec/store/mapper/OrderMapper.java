5989702491111fa77c2713ea0fc5f94cd186e2d5
Manuel Osorio <manuelosorio@usantotomas.edu.co>
2021-08-21 01:39:49 -0500

    se agrega capa mapper

---
+++ rest-api-backend/src/main/java/com/evertec/store/mapper/OrderMapper.java
@@ -0,0 +1,21 @@
+package com.evertec.store.mapper;
+
+import com.evertec.store.dto.OrderDTO;
+import com.evertec.store.persistence.entity.Order;
+
+public class OrderMapper implements ModelMapper<OrderDTO, Order>{
+
+	@Override
+	public OrderDTO mapper(Order entity) {
+//		OrderDTO orderDTO = new OrderDTO();
+//		orderDTO.setId(entity.getId());
+//		orderDTO.setCustomer_email(customer_email);(entity.getCreated_at());
+//		orderDTO.setOrderId(entity.getId());
+//		orderDTO.setOrderId(entity.getId());
+//		orderDTO.setOrderId(entity.getId());
+//		orderDTO.setOrderId(entity.getId());
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
